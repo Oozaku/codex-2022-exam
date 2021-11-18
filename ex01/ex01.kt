124a66ebfaf8d5f696e61f279d3a32c1a48344eb
Willian Ozako <willian.ozako@gmail.com>
2021-11-18 18:41:01 -0300

    Add first version of exercise 01
    Add my first approach to exercise 01, but I need to add a readme with
    instructions of how to build. Also the solution is ugly, if there is
    time, maybe use `map` and `sum` like functions as Python.

---
+++ ex01/ex01.kt
@@ -0,0 +1,54 @@
+package ex01
+
+
+fun isCnpjValid (cnpj: String) : Boolean {
+    // Cleaning cnpj from non-number characters
+    val cleanCnpj = cnpj.filter{ it.isDigit() }
+    val coef1 = listOf(5, 4, 3, 2, 9,
+                       8, 7, 6, 5, 4,
+                       3, 2, 0, 0)
+    val coef2 = listOf(6, 5, 4, 3, 2,
+                       9, 8, 7, 6, 5,
+                       4, 3, 2, 0)
+
+    // Calculate valid1 and valid2 and CNPJ is valid
+    // only if they are equal to the last two numbers of cnpj
+    var valid1 = 0
+    var valid2 = 0
+    for (idx in cleanCnpj.indices) {
+        valid1 += Character.getNumericValue(cleanCnpj[idx]) * coef1[idx]
+        valid2 += Character.getNumericValue(cleanCnpj[idx]) * coef2[idx]
+    }
+    valid1 = 11 - valid1 % 11
+    valid1 = if (valid1 >= 10) {0} else {valid1}
+    valid2 = 11 - valid2 % 11
+    valid2 = if (valid2 >= 10) {0} else {valid2}
+
+    return (valid1 == Character.getNumericValue(cleanCnpj[12])
+            && valid2 == Character.getNumericValue(cleanCnpj[13]))
+}
+
+
+
+fun main (args: Array<String> ) {
+    // Debuging isCnpjValid
+    // val cnpjs = listOf(
+    //     "43.465.089/0001-74",
+    //     "12.695.341/0001-08",
+    //     "47.726.913/0001-53",
+    //     "70933808000107",
+    //     "12512427000158",
+    //     "00.000.000/0000-00",
+    //     "12.345.678/9012-34",
+    //     "43.676.321/9642-87"
+    // )
+    // for (cnpj in cnpjs) {
+    //     println("Testing CNPJ: $cnpj")
+    //     println(isCnpjValid(cnpj))
+    // }
+    val cnpj = args[0]
+    if (isCnpjValid(cnpj))
+        println("$cnpj is valid!")
+    else
+        println("$cnpj is not valid!")
+}
