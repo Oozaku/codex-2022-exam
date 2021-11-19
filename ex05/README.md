c58971d42b8fd50ba58c55d380e03c82a85747a2
Willian Ozako <willian.ozako@gmail.com>
2021-11-19 10:58:02 -0300

    Add my first version of fifth exercise
    Add solution in Python3 to the fifth exercise, and also adding a small
    description of what it does, the packages that are required in order to
    run the script and how to use it.

---
+++ ex05/README.md
@@ -0,0 +1,23 @@
+# Description
+
+Get time in API 'http://worldclockapi.com/api/json/utc/now' and printing the time in timezone UTC and the time in my timezone, UTC-3.
+
+# Requirements
+
+To run this script, you need `Python3` and its library `requests`.
+
+# Usage
+
+Just run in your terminal:
+
+~~~sh
+python3 ex05.py
+~~~
+
+This should return something similar to the output bellow, where the first line represents the date and time in UTC and the second tells us the date and time in my local time, UTC-3.
+
+~~~sh
+UTC: 2021-11-19T13:53Z
+UTC-3: 2021-11-19T10:53Z
+~~~
+
---
+++ ex05/ex05.py
@@ -0,0 +1,69 @@
+import requests
+import sys
+
+
+def getDateTime(url, field):
+    response = requests.get(url)
+    if response.ok:
+        dateTime = response.json()[field]
+        # Extracting date, and creating a list with the format [yyyy, mm, dd]
+        date = list(map(int,
+                        dateTime.split('T')[0]
+                                .split('-')))
+        # Extracting time and creating a list with the format [hours, minutes]
+        time = list(map(int,
+                        dateTime.split('T')[1][:-1]
+                                .split(':')))
+        return date, time
+    else:
+        sys.exit(1)
+
+def convertToSpTimezone(dateUTC, timeUTC):
+    year = 0
+    month = 1
+    day = 2
+    daysInMonth = [
+        31,                                     # Jan
+        29 if dateUTC[year] % 4 == 0 else 28,   # Feb
+        31,                                     # Mar
+        30,                                     # Apr
+        31,                                     # May
+        30,                                     # Jun
+        31,                                     # Jul
+        31,                                     # Aug
+        30,                                     # Sep
+        31,                                     # Oct
+        30,                                     # Nov
+        31                                      # Dec
+    ]
+    utcSpDiff = -3
+    dateSP = dateUTC[:]
+    timeSP = timeUTC[:]
+    timeSP[0] += utcSpDiff
+    if timeSP[0] < 0:
+        timeSP[0] += 24
+        dateSP[day] -= 1
+        if dateSP[day] <= 0:
+            dateSP[month] -= 1
+            dateSP[day] += daysInMonth[dateSP[month] - 1]
+            if dateSP[month] <= 0:
+                dateSP[year] -= 1
+                dateSP[month] += 12
+    return dateSP, timeSP
+
+def dateTimeToString(date, time, timezone='TEST'):
+    date = list(map(lambda x: f"{x:02d}", date))
+    time = list(map(lambda x: f"{x:02d}", time))
+    return f"{timezone}: {'-'.join(date)}T{':'.join(time)}Z"
+
+
+if __name__ == '__main__':
+    # dateSP, timeSP = convertToSpTimezone([2000, 1, 1], [0, 0])
+    # print(dateTimeToString(dateSP, timeSP))
+    # dateSP, timeSP = convertToSpTimezone([2001, 3, 1], [2, 59])
+    # print(dateTimeToString(dateSP, timeSP))
+
+    dateUTC, timeUTC = getDateTime('http://worldclockapi.com/api/json/utc/now', 'currentDateTime')
+    dateSP, timeSP = convertToSpTimezone(dateUTC, timeUTC)
+    print(dateTimeToString(dateUTC, timeUTC, timezone='UTC'))
+    print(dateTimeToString(dateSP, timeSP, timezone='UTC-3'))
